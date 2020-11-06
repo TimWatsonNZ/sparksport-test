ebb29ecf7b00cb3aa4e4640a5a8340faa55c185d
Tim Watson <tim.watson.nz@gmail.com>
2020-11-07 11:59:20 +1300

    Adds server and api test call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
+const testApi = () => axios.get('/ping');
+
 function App() {
+  const [testResult, setTestResult] = useState('');
+
+  useEffect(() => {
+    testApi().then(result => setTestResult(result.data));
+  }, []);
+
   return (
     <div className="App">
+      <span>{testResult}</span>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
