9e50f337f8cce6dc6a6b2c23ba3d9405b7645aec
Tim Watson <tim.watson.nz@gmail.com>
2020-11-08 19:06:05 +1300

    Adds serve script and fixes test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Search Page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const searchElement = screen.getByPlaceholderText(/Search for a movie/i);
+  expect(searchElement).toBeInTheDocument();
 });
